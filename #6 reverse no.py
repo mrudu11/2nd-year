49d0b66eb19eb81ef2162d20a93ddaea158a778a
Mrudula <mrudulawaghmode11@gmail.com>
2025-04-04 07:05:31 -0700

    Add files via upload

---
+++ #6 reverse no.py
@@ -0,0 +1,7 @@
+#6 reverse no
+num = int(input("Enter a number: "))
+reversed_num=str(num)
+reversed_num=reversed_num[::-1]
+reversed_num =int(reversed_num)
+
+print("Reversed number:", reversed_num)
