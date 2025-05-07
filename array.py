739d50893a195070524c7953735a38e940daf464
Mrudula <mrudulawaghmode11@gmail.com>
2025-05-06 21:21:19 -0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([1,2,3])
+arr1 = np.array([[1,2,3],[4,5,6]])
+arr2 = np.array([[[1,2,3],[4,5,6]],[[6,7,8],[9,10,11]]])
+print(arr[2])
+print(arr1[0,2])
+print(arr2[1,0,0])
