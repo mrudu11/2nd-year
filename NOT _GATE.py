e52e5b009aa3685bb98b3f88b585d710d7b0d89e
Mrudula <mrudulawaghmode11@gmail.com>
2025-05-12 00:49:45 -0700

    Add files via upload

---
+++ NOT _GATE.py
@@ -0,0 +1,20 @@
+#implentation of NOT GATE
+def artificial_NOT(input,weight,bias):
+    #initialise sum to 0
+    weighted_sum =0
+    #calculate the weighted sum
+    for i in range(len(input)):
+      weighted_sum+=input[i] * weight[i]
+
+    #apply the activation function (step function)
+    output = 1 if weighted_sum > 0 else 0
+
+    return output
+
+    # Test the artificial neuron
+input =[1] #[0]
+weight = [-1.5]
+bias = 0.5
+result = artificial_NOT(input, weight, bias)
+
+print("Output:", result)
