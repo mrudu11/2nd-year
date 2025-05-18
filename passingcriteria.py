7cc14ff4e3baad12729cb8f10e9e76c2714a3b19
Mrudula <mrudulawaghmode11@gmail.com>
2025-05-17 23:23:32 -0700

    Add files via upload

---
+++ passingcriteria.py
@@ -0,0 +1,6 @@
+#4 students who passed
+import numpy as np
+marks = [35,60,42,75,29,90,55]
+marks_array = np.array(marks)
+passed_students =["marks of students who passed",(marks_array>=40)]
+print(passed_students)
